7a96fc5e2a004df082e6c8504120b054aa246682
muhammadSharaf <m.sharf47@gmail.com>
2019-04-08 22:43:34 +0200

    adding token generator

---
+++ TokenGenerator.cpp
@@ -0,0 +1,74 @@
+#include <utility>
+
+#include <utility>
+
+//
+// Created by sharaf on 08/04/2019.
+//
+
+#include "TokenGenerator.h"
+
+TokenGenerator::TokenGenerator(vector<vector<DFAminiState> > minimizedDfa, set<char> inputs) : minimizedDfa(
+        std::move(minimizedDfa)), inputs(std::move(inputs)) {};
+
+vector<string> TokenGenerator::generateTokens(ifstream file) {
+    string inputStream((std::istreambuf_iterator<char>(file)),
+               std::istreambuf_iterator<char>());
+    return generateTokens(inputStream);
+}
+
+vector<string> TokenGenerator::generateTokens(string inputStream) {
+    vector<string> tokens;
+
+    DFAminiState *lastAcceptingState = nullptr, *tempState;
+    int crrentStateId = STARTING_STATE_ID, inputIndex, lastAcceptingInput = 0;
+    char input;
+
+    for (int i = 0; i < inputStream.size(); i++) {
+        input = inputStream[i];
+        if (input == ' ' || input == '\t' || input == '\n') {
+            if (i == 0)
+                continue;
+            else {
+                char prev = inputStream[i - 1];
+                if (prev == ' ' || prev == '\t' || prev == '\n')
+                    continue;
+                else if (!getToken(tokens, lastAcceptingInput, lastAcceptingState, crrentStateId, i))//if not => error
+                    return tokens;
+            }
+        }
+
+        inputIndex = std::distance(inputs.begin(), inputs.find(input));
+        if (inputIndex >= inputs.size()) {
+            //input dosen't exist
+        } else {
+            tempState = &minimizedDfa[crrentStateId][inputIndex + 1];
+            if (tempState->isAccepting) {
+                lastAcceptingState = tempState;
+                lastAcceptingInput = i;
+            }
+        }
+
+        if (i == inputStream.size() - 1) {
+            if (!getToken(tokens, lastAcceptingInput, lastAcceptingState, crrentStateId, i))//if not => error
+                return tokens;
+        }
+    }
+
+    return tokens;
+}
+
+bool TokenGenerator::getToken(vector<string> &tokens, int lastAcceptingInput, DFAminiState *&lastAcceptingState,
+                              int &crrentStateId, int &i) const {
+    if (lastAcceptingState != nullptr) {
+        tokens.push_back(lastAcceptingState->stateName);
+        i = lastAcceptingInput;
+        lastAcceptingState = nullptr;
+        crrentStateId = STARTING_STATE_ID;
+        return true;
+    } else {
+        //ERROR massage
+        return false;
+    }
+}
+
